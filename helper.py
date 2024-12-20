7f7363e6b59e2dcd64cb43c569f57647c7515bdb
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-20 13:33:17 +0700

    add helper to load distance matrix

--- helper.py
+++ helper.py
@@ -2,10 +2,16 @@ import random
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
 
 def get_random_competitor(n_nodes):
     return random.randint(0, n_nodes - 1)
+
+
+def load_distance_matrix_from_csv(file_path):
+    d = np.loadtxt(file_path, delimiter=",", dtype=int)
+    return d
 
 
 def visualize(net):
