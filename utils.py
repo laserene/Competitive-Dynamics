7966ac36e7f45802e6c6b88c0dd3ef6ac5ab81c0
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-03-27 18:54:18 +0700

    utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import random
+
+
+def sign(x):
+    try:
+        return 1 if x >= 0 else -1
+    except:
+        return 1
+
+
+def get_random_competitor(n_nodes):
+    return random.randint(0, n_nodes - 1)
