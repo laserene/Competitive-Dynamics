d797dae5db03ffa0a53118a4d3b7e12f1d4aa780
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-03-28 23:28:58 +0700

    add id to each node

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 from networkx import MultiGraph
+from biomart import BiomartServer
 
 
 def import_network_from_file(filename):
@@ -7,11 +8,14 @@ def import_network_from_file(filename):
 
     network = MultiGraph()
     for line in data[1:]:
-        from_node, to_node, _, _ = line.strip().split("\t")
-        # direction = int(direction)
-        # weight = int(weight)
-
+        from_node, to_node, direction, weight = line.strip().split("\t")
+        direction = int(direction)
+        weight = int(weight)
         network.add_edge(from_node, to_node)
+
+    # Assign unique IDs (e.g., integer IDs)
+    for i, node in enumerate(network.nodes()):
+        network.nodes[node]["id"] = i  # Assign an integer ID
 
     return network
 
@@ -25,3 +29,17 @@ def is_undirected_graph(filename):
         if direction == '1':
             return False
     return True
+
+
+def load_network_weight(path):
+    pass
+
+
+def convert_ensembl_gene_id_to_symbol(ensembl_gene_id):
+    server = BiomartServer("http://www.ensembl.org/biomart")
+    mart = server.datasets["hsapiens_gene_ensembl"]
+    result = mart.search({
+        'filters': {'ensembl_gene_id': ["ENSG00000169242"]},
+        'attributes': ["ensembl_gene_id", "external_gene_name"]
+    })
+    return result.text
