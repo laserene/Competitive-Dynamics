67f6add3cfa5382798cb5fad8754071f16187aea
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-04-02 19:56:32 +0700

    update the consensus distributed protocol

--- outside_competition.py
+++ outside_competition.py
@@ -1,10 +1,27 @@
 import os
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 
 import networkx as nx
 import numpy as np
 from tqdm import tqdm
 
 INF = 10000
+
+
+def retrieve_initial_state(network):
+    state = {}
+    for node in network.nodes:
+        state[node] = network.nodes[node]['score']
+
+    return state
+
+
+def get_max_deg(network):
+    max_deg = 0
+    for _, degree in network.degree:
+        max_deg = max(max_deg, degree)
+    return max_deg
 
 
 def compute_distance_matrix(dataset, network):
@@ -31,12 +48,63 @@ def load_distance_matrix_from_file(filepath):
     return distance_matrix
 
 
-def compete(network, alpha):
-    n = len(network.nodes)
-    pass
+def compete(alpha, network, co_expression, node_set, max_deg):
+    # Hyperparameters
+    epsilon = 2 * 1e-7
+    max_iterations = len(node_set) * len(network.edges)
+    mul_coeff = 1 / max_deg
+
+    state = retrieve_initial_state(network)
+
+    for y in tqdm(node_set):
+        # Initially, set of nodes doesn't contain beta
+        if y == alpha:
+            continue
+
+        # Outside competitors initialization
+        beta = 'BETA'
+
+        state[alpha] = 1
+        state[beta] = -1
+
+        # Append (Beta, node) edge. Edge can only be appended once node score are retrieved
+        network.add_edge(beta, y)
+
+        t = 0
+        while True:
+            converging = 0
+            for u in node_set:
+                # Initially, set of nodes doesn't contain beta
+                if u == alpha:
+                    continue
+
+                s = 0
+                for v in network.neighbors(u):
+                    co_expression_score = min(co_expression.get(f'{u}, {v}', 1), co_expression.get(f'{v}, {u}', 1))
+
+                    if v != beta:
+                        s += (1 + co_expression_score) * (state[v] - state[u])
+                    else:
+                        s += state[v] - state[u]
+
+                old_state = state[u]
+                state[u] += mul_coeff * s
+                converging += np.abs(old_state - state[u])
+
+            t += 1
+            if converging > epsilon and t < max_iterations:
+                break
+
+        network.remove_edge(beta, y)
+        network.remove_node(beta)
+
+    return state
 
 
 def compute_influence_matrix(states, distance_matrix, node_dict):
+    """
+        Deprecated method
+    """
     n_nodes = len(node_dict)
     influence_matrix = np.zeros((n_nodes, n_nodes))
 
@@ -51,6 +119,9 @@ def compute_influence_matrix(states, distance_matrix, node_dict):
 
 
 def compute_total_support(alpha_id, influence_matrix, node_dict, state):
+    """
+        Deprecated method
+    """
     def sign(value):
         if value > 0:
             return 1
@@ -71,4 +142,15 @@ def compute_total_support(alpha_id, influence_matrix, node_dict, state):
 
 
 def outside_competition(network, distance_matrix, co_expression):
-    pass
+    print("Competition in progress...")
+    node_set = list(network.nodes)
+    max_deg = get_max_deg(network)
+    candidates = node_set[:1000]
+
+    with ProcessPoolExecutor() as executor:
+        states = list(tqdm(executor.map(
+            partial(compete, network=network, co_expression=co_expression,
+                    node_set=node_set, max_deg=max_deg),
+            candidates)))
+
+    return states
