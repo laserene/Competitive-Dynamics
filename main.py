4a796ea8c623682e5452d6e7c8e3d691cc9d6a91
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-04-02 19:57:52 +0700

    temporarily save post-competiton state to a file

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main():
     distance_matrix_path = './distance_matrix/Human PPI network_distance_matrix.csv'
     gene_score_path = './resource/differentially_expressed_genes/TCGA-BRCA_de_genes.json'
     co_expression_path = './resource/co_exp_network/TCGA-BRCA__co_expression__t_70_.json'
-    support_path = './support/ppi_total_support.json'
+    state_path = './support/ppi_post_competition_state.json'
 
     network, co_expression = import_network_from_file(filepath, gene_score_path, co_expression_path)
     distance_matrix = load_distance_matrix_from_file(distance_matrix_path)
 
     states = outside_competition(network, distance_matrix, co_expression)
-    with open(support_path, 'w') as f:
+    with open(state_path, 'w') as f:
         json.dump(states, f)
 
 
