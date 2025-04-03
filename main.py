70eb4db3ad6b630ae8666623be08da6bab008038
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-04-03 21:25:24 +0700

    update state result file naming

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     distance_matrix_path = './distance_matrix/Human PPI network_distance_matrix.csv'
     gene_score_path = './resource/differentially_expressed_genes/TCGA-BRCA_de_genes.json'
     co_expression_path = './resource/co_exp_network/TCGA-BRCA__co_expression__t_70_.json'
-    state_path = './support/ppi_post_competition_state.json'
 
     network, co_expression = import_network_from_file(filepath, gene_score_path, co_expression_path)
     try:
@@ -20,8 +19,12 @@ def main():
         print('Distance matrix file does not exist!')
         distance_matrix = compute_distance_matrix(dataset, network)
 
+    start = 1000
+    end = 1300
+    states = outside_competition(network, co_expression, start, end)
+
     os.makedirs("support", exist_ok=True)
-    states = outside_competition(network, distance_matrix, co_expression)
+    state_path = f'./support/ppi_post_competition_state_{start}_{end}".json'
     with open(state_path, 'w') as f:
         json.dump(states, f)
 
