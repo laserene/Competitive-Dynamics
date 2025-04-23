fc49b8c080e7835b8ecf06e8e5ea837d51eb8674
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-04-23 15:48:01 +0700

    minor changes

--- compare.py
+++ compare.py
@@ -4,26 +4,25 @@ import pandas as pd
 
 
 def main():
-    files = os.listdir("./total_support")
-    db = pd.read_csv("./OncoKB_geneList.csv")[["Hugo Symbol", "Entrez Gene ID", "Is Tumor Suppressor Gene"]]
+    n_gene = 3
+    files = os.listdir("./supports")
+    db = pd.read_csv("./OncoKB_geneList.csv")[["Hugo Symbol", "Entrez Gene ID"]]
     for file in files:
-        data = pd.read_csv(f"./total_support/{file}")
-        max_support = data.iloc[:, 2].min()
-        genes = data[data.iloc[:, 2] == max_support].iloc[:, 1].values.tolist()
+        total_supports = pd.read_csv(f"./supports/{file}")
+        max_n_supported_genes = total_supports.sort_values(by=total_supports.columns[2], ascending=False)[
+                                :n_gene].iloc[:, 1].values.tolist()
 
-        # Get the top 10 genes if there are less than 10 genes with the same states
-        if len(genes) < 10:
-            genes = data.sort_values(by=data.columns[2], ascending=True).iloc[:10, 1].values.tolist()
         results = []
-
-        for gene in genes:
+        for gene in max_n_supported_genes:
             gene_id = gene.split("hsa:")[1]
             oncokb_gene = db[db["Entrez Gene ID"] == int(gene_id)]
             if oncokb_gene.empty:
-                results.append(f"{gene_id} {data[data.iloc[:, 1] == gene].iloc[:, 2].values.tolist()[0]}\n")
+                results.append(
+                    f"{gene_id} {total_supports[total_supports.iloc[:, 1] == gene].iloc[:, 2].values.tolist()[0]}\n")
             else:
                 gene_name = oncokb_gene["Hugo Symbol"].values[0]
-                results.append(f"\\textbf{{{gene_name}}} {data[data.iloc[:, 1] == gene].iloc[:, 2].values.tolist()[0]}\n")
+                results.append(
+                    f"\\textbf{{{gene_name}}} {total_supports[total_supports.iloc[:, 1] == gene].iloc[:, 2].values.tolist()[0]}\n")
 
         # Write to file
         filename = file.split(".")[0].split("_total_support")[0]
